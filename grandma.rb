0664a2fda1f052a161c70a28a1db9c569b1c9285
Tiffany Carter <tafby88@gmail.com>
2018-10-19 18:31:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(string)
   if string != string.upcase
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif string == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-else
-  puts "NO, NOT SINCE 1938!"
-end
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "NO, NOT SINCE 1938!"
+  end
 end
